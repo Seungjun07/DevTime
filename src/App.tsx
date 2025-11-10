dd15b7b0de918076055f811f156b84371e3d006c
SeungJun <yangsj8989@naver.com>
2025-11-10 16:54:42 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+import MyPage from "./pages/my-page";
+import ProfileDetailPage from "./pages/profile-detail-page";
+
 function App() {
-  return <div className="font-bold">hello world</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="/sign-up" element={<SignInPage />} />
+      <Route path="/sign-in" element={<SignUpPage />} />
+      <Route path="/my-page" element={<MyPage />} />
+      <Route path="/profile" element={<ProfileDetailPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/sign-in-page.tsx
@@ -0,0 +1,3 @@
+export default function SignInPage() {
+  return <div>sing in page</div>;
+}
